73ffb336fc9aa5381d47ee674d0c9c064775a500
Franciszek Sikora <sikorafranek@gmail.com>
2025-05-07 08:19:10 +0200

    added redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MinecraftModule } from './minecraft/minecraft.module';
 
 @Module({
-  imports: [MinecraftModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 10, // 10 seconds cache
+    }),
+    MinecraftModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/minecraft/minecraft.controller.ts
+++ src/minecraft/minecraft.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 import { MinecraftService } from './minecraft.service';
 
 @Controller('minecraft')
+@UseInterceptors(CacheInterceptor)
 export class MinecraftController {
   constructor(private readonly minecraftService: MinecraftService) {}
 
   @Get('status')
+  @CacheKey('minecraft-status')
+  @CacheTTL(10)
   async getStatus(@Query('ip') ip: string) {
     if (!ip) {
       throw new BadRequestException('Parametr "ip" jest wymagany');
